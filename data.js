fae0c5f421d81153df0187d75bf05d77c977846d
mebliromax <maruschakson@gmail.com>
2026-03-17 16:00:49 +0200

    ааа

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const blocksData = [
     {
         mainImage: "./images/h1.jpeg",
-        imagePosition: "center",
+        imagePosition: "60%",
         imageContain: false,
         banner: {
             title: "LOFT",
@@ -10,7 +10,7 @@ const blocksData = [
         products: [
             {
                 image: "./images/p1.jpg",
-                title: "Тумба Loft 1",
+                title: "Тумба Loft 1400",
                 specs: ["ширина - 1400мм", "висота - 500мм", "глибина - 450мм"],
                 oldPrice: "8200₴",
                 newPrice: "7380₴"
@@ -57,7 +57,164 @@ const blocksData = [
         ]
     },
     {
-        mainImage: "./images/h3.jpeg",
+        mainImage: "./images/h4.jpeg",
+        imagePosition: "center",
+        imageContain: true,
+        banner: {
+            title: "LOFT",
+            description: "Корпус: сертифіковане ламіноване ДСП Kronospan<br>Метал: порошкове фарбування<br>Фурнітура: GTV (Польща)"
+        },
+        products: [
+            {
+                image: "./images/p9.jpg",
+                title: "Пенал Loft",
+                specs: ["ширина - 500мм", "висота - 2000мм", "глибина - 500мм"],
+                oldPrice: "8900₴",
+                newPrice: "8010₴"
+            },
+            {
+                image: "./images/p10.jpg",
+                title: "Передпокій Loft",
+                specs: ["ширина - 600мм", "висота - 2000 мм", "глибина - 450мм"],
+                oldPrice: "12900₴",
+                newPrice: "11610₴"
+            }
+        ]
+    },
+    {
+        mainImage: "./images/h5.jpeg",
+        imagePosition: "70%",
+        imageContain: false,
+        banner: {
+            title: "LOFT",
+            description: "Корпус: сертифіковане ламіноване ДСП Kronospan<br>Метал: порошкове фарбування<br>Фурнітура: GTV (Польща)"
+        },
+        products: [
+            {
+                image: "./images/p11.jpg",
+                title: "Ліжко Loft 160x200",
+                specs: ["ширина - 1680мм", "висота - 860мм", "глибина - 2060мм"],
+                oldPrice: "11900₴",
+                newPrice: "10710₴"
+            },
+            {
+                image: "./images/p12.jpg",
+                title: "Тумба приліжкова Loft",
+                specs: ["ширина - 500мм", "висота - 500 мм","глибина - 450 мм"],
+                oldPrice: "4500₴",
+                newPrice: "4050₴"
+            }
+        ]
+    },
+    {
+        mainImage: "./images/h6.jpeg",
+        imagePosition: "80%",
+        imageContain: false,
+        banner: {
+            title: "LOFT",
+            description: "Корпус: сертифіковане ламіноване ДСП Kronospan<br>Метал: порошкове фарбування<br>Фурнітура: GTV (Польща)"
+        },
+        products: [
+            {
+                image: "./images/p7.jpg",
+                title: "Ліжко Loft 160x200",
+                specs: ["ширина - 1680мм", "висота - 860мм", "глибина - 2060мм"],
+                oldPrice: "11900₴",
+                newPrice: "10710₴"
+            },
+            {
+                image: "./images/p8.jpg",
+                title: "Тумба приліжкова Loft",
+                specs: ["ширина - 500мм", "висота - 500 мм","глибина - 450 мм"],
+                oldPrice: "4500₴",
+                newPrice: "4050₴"
+            }
+        ]
+    },
+    {
+        mainImage: "./images/h7.jpeg",
+        imagePosition: "70%",
+        imageContain: false,
+        banner: {
+            title: "LOFT",
+            description: "Корпус: сертифіковане ламіноване ДСП Kronospan<br>Метал: порошкове фарбування<br>Фурнітура: GTV (Польща)"
+        },
+        products: [
+            {
+                image: "./images/p13.jpg",
+                title: "Тумба Loft 1400",
+                specs: ["ширина - 1400мм", "висота - 500мм", "глибина - 450мм"],
+                oldPrice: "8200₴",
+                newPrice: "7380₴"
+            },
+            {
+                image: "./images/p14.jpg",
+                title: "Дзеркало LOFT",
+                specs: ["ширина - 800мм", "висота - 1000 мм"],
+                oldPrice: "4200₴",
+                newPrice: "3780₴"
+            },
+            {
+                image: "./images/p15.jpg",
+                title: "Стіл LOFT 1ш",
+                specs: ["ширина - 900мм", "висота - 750мм", "глибина - 600мм"],
+                oldPrice: "6400₴",
+                newPrice: "5760₴"
+            }
+        ]
+    },
+    {
+        mainImage: "./images/h8.jpeg",
+        imagePosition: "center",
+        imageContain: true,
+        banner: {
+            title: "LOFT",
+            description: "Корпус: сертифіковане ламіноване ДСП Kronospan<br>Метал: порошкове фарбування<br>Фурнітура: GTV (Польща)"
+        },
+        products: [
+            {
+                image: "./images/p16.jpg",
+                title: "Шафа 3Д Loft (Дзеркало)",
+                specs: ["ширина - 1400мм", "висота - 2000мм", "глибина - 500мм"],
+                oldPrice: "19900₴",
+                newPrice: "17910₴"
+            },
+            {
+                image: "./images/p5.jpg",
+                title: "Передпокій Loft 600",
+                specs: ["ширина - 600мм", "висота - 2000 мм", "глибина - 450мм"],
+                oldPrice: "12900₴",
+                newPrice: "11610₴"
+            }
+        ]
+    },
+    {
+        mainImage: "./images/h9.jpeg",
+        imagePosition: "center",
+        imageContain: true,
+        banner: {
+            title: "LOFT",
+            description: "Корпус: сертифіковане ламіноване ДСП Kronospan<br>Метал: порошкове фарбування<br>Фурнітура: GTV (Польща)"
+        },
+        products: [
+            {
+                image: "./images/p17.jpg",
+                title: "Шафа 2Д Loft",
+                specs: ["ширина - 900мм", "висота - 2000мм", "глибина - 500мм"],
+                oldPrice: "10900₴",
+                newPrice: "9810₴"
+            },
+            {
+                image: "./images/p5.jpg",
+                title: "Передпокій Loft 600",
+                specs: ["ширина - 600мм", "висота - 2000 мм", "глибина - 450мм"],
+                oldPrice: "12900₴",
+                newPrice: "11610₴"
+            }
+        ]
+    },
+    {
+        mainImage: "./images/h10.jpeg",
         imagePosition: "center",
         imageContain: false,
         banner: {
@@ -66,26 +223,133 @@ const blocksData = [
         },
         products: [
             {
-                image: "./images/p6.jpg",
+                image: "./images/p18.jpg",
+                title: "Шафа 3Д Loft",
+                specs: ["ширина - 1400мм", "висота - 2000мм", "глибина - 500мм"],
+                oldPrice: "15900₴",
+                newPrice: "14310₴"
+            },
+            {
+                image: "./images/p10.jpg",
+                title: "Передпокій Loft 600",
+                specs: ["ширина - 600мм", "висота - 2000 мм", "глибина - 450мм"],
+                oldPrice: "12900₴",
+                newPrice: "11610₴"
+            }
+        ]
+    },
+    {
+        mainImage: "./images/h11.jpeg",
+        imagePosition: "70%",
+        imageContain: false,
+        banner: {
+            title: "LOFT",
+            description: "Корпус: сертифіковане ламіноване ДСП Kronospan<br>Метал: порошкове фарбування<br>Фурнітура: GTV (Польща)"
+        },
+        products: [
+            {
+                image: "./images/p19.jpg",
+                title: "Ліжко Loft 90x200",
+                specs: ["ширина - 990мм", "висота - 860мм", "глибина - 2060мм"],
+                oldPrice: "7900₴",
+                newPrice: "7110₴"
+            },
+            {
+                image: "./images/p8.jpg",
+                title: "Тумба приліжкова Loft",
+                specs: ["ширина - 500мм", "висота - 500 мм", "глибина - 450мм"],
+                oldPrice: "4500₴",
+                newPrice: "4050₴"
+            }
+        ]
+    },
+    {
+        mainImage: "./images/h3.jpeg",
+        imagePosition: "70%",
+        imageContain: false,
+        banner: {
+            title: "LOFT",
+            description: "Корпус: сертифіковане ламіноване ДСП Kronospan<br>Метал: порошкове фарбування<br>Фурнітура: GTV (Польща)"
+        },
+        products: [
+            {
+                image: "./images/p7.jpg",
                 title: "Ліжко LOFT 160х200",
                 specs: ["ширина - 1680мм", "висота - 860мм", "глибина - 2060мм"],
                 oldPrice: "11900₴",
                 newPrice: "10710₴"
             },
             {
-                image: "./images/p7.jpg",
+                image: "./images/p8.jpg",
                 title: "Тумба приліжкова LOFT",
                 specs: ["ширина - 500мм", "висота - 500 мм", "глибина - 450мм"],
                 oldPrice: "4500₴",
-                newPrice: "4050"
+                newPrice: "4050₴"
+            },
+            {
+                image: "./images/p6.jpg",
+                title: "Ліжко двоярусне LOFT 80х190",
+                specs: ["ширина - 1980мм", "висота - 1500 мм", "глибина - 850мм"],
+                oldPrice: "13900₴",
+                newPrice: "12510₴"
+            }
+        ]
+    },
+    {
+        mainImage: "./images/h12.jpeg",
+        imagePosition: "70%",
+        imageContain: false,
+        banner: {
+            title: "LOFT",
+            description: "Корпус: сертифіковане ламіноване ДСП Kronospan<br>Метал: порошкове фарбування<br>Фурнітура: GTV (Польща)"
+        },
+        products: [
+            {
+                image: "./images/p6.jpg",
+                title: "Ліжко двоярусне Loft 80x190",
+                specs: ["ширина - 1980мм", "висота - 1500мм", "глибина - 850мм"],
+                oldPrice: "13900₴",
+                newPrice: "12510₴"
             },
             {
                 image: "./images/p8.jpg",
-                title: "Ліжко двоярусне LOFT 80х1900",
-                specs: ["ширина - 1980мм", "висота - 1500 мм", "глибина - 850мм"],
-                oldPrice: "13900₴",
-                newPrice: "12510"
+                title: "Тумба приліжкова Loft",
+                specs: ["ширина - 500мм", "висота - 500 мм", "глибина - 450мм"],
+                oldPrice: "4500₴",
+                newPrice: "4050₴"
             }
         ]
-    }
+    },
+    {
+        mainImage: "./images/h13.jpeg",
+        imagePosition: "center",
+        imageContain: false,
+        banner: {
+            title: "LOFT",
+            description: "Корпус: сертифіковане ламіноване ДСП Kronospan<br>Метал: порошкове фарбування<br>Фурнітура: GTV (Польща)"
+        },
+        products: [
+            {
+                image: "./images/p4.jpg",
+                title: "Передпокій Loft 600",
+                specs: ["ширина - 600мм", "висота - 2000мм", "глибина - 450 мм"],
+                oldPrice: "12900₴",
+                newPrice: "11610₴"
+            },
+            {
+                image: "./images/p20.jpg",
+                title: "Тумба Loft 1800",
+                specs: ["ширина - 1800мм", "висота - 500 мм", "глибина - 450 мм"],
+                oldPrice: "9900₴",
+                newPrice: "8910₴"
+            },
+            {
+                image: "./images/p21.jpg",
+                title: "Стелаж Loft 600",
+                specs: ["ширина - 600мм", "висота - 2000мм", "глибина - 500мм"],
+                oldPrice: "7200₴",
+                newPrice: "6480₴"
+            }
+        ]
+    },
 ];
